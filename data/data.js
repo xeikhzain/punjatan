709f4e6898cf71a8bca04c927f3fcc732deec429
ikram-09 <im21560282gmail.com>
2024-03-18 16:10:03 +0500

    ikram side work

---
+++ data/data.js
@@ -0,0 +1,165 @@
+const dealOfDay = [
+    {
+        image: '../Punjatan Assets/timthumb (2).jpg',
+        aboutProduct: 'Beautiful “Masha Allah” Calligraphy Work Frame',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+    {
+        image: '../Punjatan Assets/Rectangle 7 (4).png',
+        aboutProduct: 'Rings & Jewellery',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+    {
+        image: '../Punjatan Assets/cart 2.png',
+        aboutProduct: 'Graceful-Looking Red Velvet Jai-Namaz',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+    {
+        image: '../Punjatan Assets/149040-quran-arabic-holy-free-photo.png',
+        aboutProduct: 'Beautiful “Masha Allah” Calligraphy Work Frame',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+];
+
+const shopProduct = [
+    {
+        image: '../Punjatan Assets/Rectangle 7 (4).png',
+        aboutProduct: 'Natural Ruby African Yaqoot',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+    {
+        image: '../Punjatan Assets/cart 2.png',
+        aboutProduct: 'Graceful-Looking Red Velvet Jai-Namaz',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+    {
+        image: '../Punjatan Assets/149040-quran-arabic-holy-free-photo.png',
+        aboutProduct: 'The Holy Quran Majeed with Tajweed Indicating Colors',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+    {
+        image: '../Punjatan Assets/timthumb (2).jpg',
+        aboutProduct: 'Beautiful “Masha Allah” Calligraphy Work Frame',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+    {
+        image: '../Punjatan Assets/Rectangle 7 (4).png',
+        aboutProduct: 'Natural Ruby African Yaqoot',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+    {
+        image: '../Punjatan Assets/cart 2.png',
+        aboutProduct: 'Graceful-Looking Red Velvet Jai-Namaz',
+        name: 'Rings & Jewellery',
+        dec: 'The primary purpose of a "nazar band" is to protect. The Primary p...',
+        rating: '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star checked"></i>' +
+                '<i class="fa-solid fa-star"></i>' +
+                '<i class="fa-solid fa-star"></i>',
+        ratingNum: 0.4,
+        review: '96 Reviews',
+        Transactions: '273 Sold',
+        price: '$25.00',
+        carImg: '../Punjatan Assets/Vector (1).png'
+    },
+]
